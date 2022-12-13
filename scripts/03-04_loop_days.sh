db11b14ba415b3d99906f3115b70ca33fd76b8eb
elkir <sirelkir@gmail.com>
2022-12-13 07:40:27 +0000

    first loop request

--- scripts/03-04_loop_days.sh
+++ scripts/03-04_loop_days.sh
@@ -1,7 +1,7 @@
 # loop through every date in the year 2017 in the format YYYY-MM-DD and print it if the date exists and is Monday or Thursday
 # print Thu for Thursday and Mon for Monday
-# mkdir logs/v03-04_$1
-# touch logs/v03-04_$1/failed_requests.log
+mkdir logs/v03-04_$1
+touch logs/v03-04_$1/failed_requests.log
 
 # catch force flag
 if [[ $4 == "-f" ]]; then
@@ -22,9 +22,12 @@ for i in $(eval echo {$1..$1}-{$2..$3}-{01..31}); do
                     continue
                 fi
                 # request data for date $i
-                { echo "data/mars_v04${x}_${i}_$(date -d $i +%a).grib"; 
-                ./scripts/03-04_mars.sh request v03 $x $i || 
-                    echo "v03_$x_$i" >> logs/v03-04_$1/failed_requests.log; } &
+                { 
+                echo "data/mars_v04${x}_${i}_$(date -d $i +%a).grib" | # print bold and colored
+                    sed -e "s/^/$(tput bold)/" -e "s/$/$(tput sgr0)/" \
+                        -e "s/^/$(tput setaf 2)/" -e "s/$/$(tput sgr0)/";
+                ./scripts/03-04_mars.sh -N request v04$x $i || 
+                    echo "v04${x}_$i" >> logs/v03-04_$1/failed_requests.log; }
             done
         fi
     fi
