faebf5524c87881b09a9612153023cd7b73e7d4b
Petr Doležal <sirelkir@gmail.com>
2023-01-22 23:56:01 +0000

    attempts at syntax error bug fixes

--- scripts/03-04_mars.sh
+++ scripts/03-04_mars.sh
@@ -155,17 +155,17 @@ EOF
                 sed -n "s/^size=\([0-9]*\);/\1/p" | awk "{print int(\$1/61*2.88)}")
             echo "The file size should be $(numfmt --to=iec $filesize)"
             # convert filesize to GB and check if enough storage available
-            if (( $(echo "$filesize/1024^3>$storage" |bc) ))
+            if (( $(echo "$filesize/1024^3>$storage" | bc) ))
                 then
                     echo "WARNING: Not enough storage available, entering grace limit" | sed -e "s/^/$(tput setaf 1)/" -e "s/$/$(tput sgr0)/"
-                    if (( $(echo "$filesize/1024^3>$storage_max" |bc) ))
+                    if (( $(echo "$filesize/1024^3>$storage_max" | bc) ))
                         then
                             echo "ERROR: Not enough storage available, exiting process" | sed -e "s/^/$(tput setaf 1)/" -e "s/$/$(tput sgr0)/"
                             exit 1
                     fi
             fi
-    
-            rm /tmp/$filename_date.list_cost
+
+            rm "/tmp/$filename_date.list_cost"
     fi
     #print start line in colour
     echo "--------------start -----------------" | sed -e "s/^/$(tput setaf 3)/" -e "s/$/$(tput sgr0)/"
