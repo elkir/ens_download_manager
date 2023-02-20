8f740b1ebfb201df2b9a83297b9a2742b91cdd64
Petr Doležal <sirelkir@gmail.com>
2023-02-20 00:51:01 +0000

    script 03-04_loop fixed errors

--- scripts/03-04_loop_days.sh
+++ scripts/03-04_loop_days.sh
@@ -28,14 +28,15 @@ for i in $(eval echo {$1..$1}-{$2..$3}-{01..31}); do
             for x in e d;
             do
                 # skip if file already exists and force flag is not set
-                if [ -f "data/mars_v04${x}_${i}_$(date -d $i +%a).grib" ] && ! $force
+                if [ -f "data/mars_v04${x}_${i}_$(date -d $i +%a).grib" ] && [ $force == false ];
                     then
                     echo "data/mars_v04${x}_${i}_$(date -d $i +%a).grib already exists"
                     continue
                 fi
                 # save the request paramters to an array
-                params+=("$i $x")
+                params+=("$i,$x")
                 files+=("mars_v04${x}_${i}_$(date -d $i +%a).grib")
+                
             done
         fi
     fi
@@ -51,34 +52,34 @@ curl -s "$TELEGRAM_URL&text=$MESSAGE"
 
 # loop through the list of request parameters and run the script
 for param in ${params[@]}; do
-    echo $param
-    i=$(echo $param | cut -d' ' -f1)
-    x=$(echo $param | cut -d' ' -f2)
-    echo "i: $i, x: $x"
-    # # print the file name
-    # echo "data/mars_v04${x}_${i}_$(date -d $i +%a).grib" | 
-    #     # print bold and colored
-    #     sed -e "s/^/$(tput bold)/" -e "s/$/$(tput sgr0)/" \
-    #         -e "s/^/$(tput setaf 2)/" -e "s/$/$(tput sgr0)/";
-    # ./scripts/03-04_mars.sh -N request v04$x $i || 
-    #     echo "v04${x}_$i" >> logs/v03-04_$1/failed_requests.log
-# done
+    i=$(echo $param | cut -d',' -f1)
+    x=$(echo $param | cut -d',' -f2)
+    # print the file name
+    echo "data/mars_v04${x}_${i}_$(date -d $i +%a).grib" | 
+        # print bold and colored
+        sed -e "s/^/$(tput bold)/" -e "s/$/$(tput sgr0)/" \
+            -e "s/^/$(tput setaf 2)/" -e "s/$/$(tput sgr0)/";
+    ./scripts/03-04_mars.sh -N request v04$x $i || 
+        echo "v04${x}_$i" >> logs/v03-04_$1/failed_requests.log
+done
 
 # check if all files exist and print missing files
 echo "Checking if all files exist:" | 
     sed -e "s/^/$(tput bold)/" -e "s/$/$(tput sgr0)/" \
         -e "s/^/$(tput setaf 2)/" -e "s/$/$(tput sgr0)/";
 missing_files=()
+
+
 for file in ${files[@]}; do
     if [ ! -f "data/$file" ]; then
         echo "$file does not exist"
         missing_files+=("$file")
     fi
+done
 
 # if all the processes are done, then send a notification to telegram
 MESSAGE="MARS Loop through year $1 for months $2 to $3 DONE at $(hostname).hpc
 Files unsuccesful:
 $(printf "%s\n" "${missing_files[@]}")"
-wait &&
-#  curl -s "$TELEGRAM_URL&text=$MESSAGE"
-echo -e 'n'
+wait && curl -s "$TELEGRAM_URL&text=$MESSAGE"
+echo -e '\n'
